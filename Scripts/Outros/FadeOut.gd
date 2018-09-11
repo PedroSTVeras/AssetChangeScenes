99441b489ff909af2ad120f1d426df9af0dcea18
Pedro Veras <pedroveras12@gmail.com>
2018-09-11 09:46:46 -0300

    comentario

--- Scripts/Outros/FadeOut.gd
+++ Scripts/Outros/FadeOut.gd
@@ -6,6 +6,8 @@ onready var poly = $Polygon2D
 func _ready():
 	pass
 
+	#aaaaa
+	
 func _physics_process(delta):
 	poly.color.a = timer.wait_time - timer.get_time_left()
 	if timer.get_time_left() < 0.3:
